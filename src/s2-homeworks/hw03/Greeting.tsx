a377e751ccd8182b7333f24dc49e90a60aee1c48
Vasili Karpenkin <v.karpenkin@gmail.com>
2023-01-20 18:18:49 +0300

    20.01.2023 HW3 N4

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -57,7 +57,7 @@ const Greeting: React.FC<GreetingPropsType> = (
                     className={s.button}
                     // disabled={!name.trim()}
                 >
-                    add
+                    Add
                 </button>
             </div>
 
