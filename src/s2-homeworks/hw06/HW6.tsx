6fcf99788289f2da64915a432da2462683021d56
Vasili Karpenkin <v.karpenkin@gmail.com>
2023-02-09 23:26:30 +0300

    09.02.2023 HW6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,7 +18,7 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        restoreState<string>('hw6-editable-span-value', value)
+        setValue(restoreState<string>('hw6-editable-span-value', value))
     }
 
     return (
